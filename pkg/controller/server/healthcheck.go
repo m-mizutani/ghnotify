1a52c539f26247e94ea61366ddf3ff5df13c12a5
Akira Hayashi <akira.hayashi@route06.co.jp>
2023-07-20 14:36:45 +0900

    Add endpoint `/health` for health checking

---
+++ pkg/controller/server/healthcheck.go
@@ -0,0 +1,10 @@
+package server
+
+import "net/http"
+
+func handleHealthCheckRequest() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}
+}
